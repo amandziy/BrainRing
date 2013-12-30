636222512df865073ab2bdf15a5a11e11ab5bba6
Andriy <amandziy@gmail.com>
2013-12-30 22:15:49 +0200

    main.cpp initialization

--- main.cpp
+++ main.cpp
@@ -1,28 +1,53 @@
 #include <QApplication>
 #include <QStyleFactory>
+#include <QTextCodec>
 
 #include "dashboard.h"
 
 #include "api/lptapi.h"
 #include "api/lptapifactory.h"
 
-int main(int argc, char *argv[])
-{
-    QApplication application(argc, argv);
-
-    //Apply palette and style
-    QApplication::setStyle(QStyleFactory::create("Fusion"));
-    QPalette stylePalette = application.palette();
+/**
+ * @brief applyGlobalAppStyle - set application wide style.
+ * @param app - the QApplication instance
+ */
+void applyGlobalAppStyle(QApplication *application) {
+    application->setStyle(QStyleFactory::create("Fusion"));
+    QPalette stylePalette = application->palette();
       stylePalette.setColor(QPalette::Window, QColor(53,53,53));
       stylePalette.setColor(QPalette::Button, QColor(53,53,53));
       stylePalette.setColor(QPalette::Highlight, QColor(142,45,197));
       stylePalette.setColor(QPalette::ButtonText, QColor(255,255,255));
-      application.setPalette(stylePalette);
+      application->setPalette(stylePalette);
+}
+
+/**
+ * @brief applyCharsetEncoding - Setting application charset encoding.
+ * @param charset - the encoding name
+ */
+void applyCharsetEncoding(const char* charset) {
+    QTextCodec *codec = QTextCodec::codecForName(charset);
+    QTextCodec::setCodecForLocale(codec);
+}
+
+/**
+ * @brief main
+ * @param argc - // -
+ * @param argv - // -
+ * @return execute result code
+ */
+int main(int argc, char *argv[])
+{
+    QApplication *application = new QApplication(argc, argv);
+
+    applyGlobalAppStyle(application);
+
+    applyCharsetEncoding("UTF-8");
 
     LptApi *api = (new LptApiFactory())->getLptPortApi();
 
     Dashboard dashboard;
     dashboard.show();
 
-    return application.exec();
+    return application->exec();
 }
